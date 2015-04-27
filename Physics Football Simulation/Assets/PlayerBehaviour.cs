947e63de543426e6ea68ba38b0000f3d0f6962e7
maxsegovia <maxsegoviamtz@gmail.com>
2015-04-27 10:27:49 -0500

    QB movement and animation script

---
+++ Physics Football Simulation/Assets/PlayerBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerBehaviour : MonoBehaviour {
+
+	float moveSpeed = 5f;
+
+	bool move = true;
+
+	void Update () {
+
+		if (move == true) {
+			transform.Translate (-Vector3.forward * moveSpeed * Time.deltaTime);
+			if (transform.position.z >= -5) {
+				GetComponent<Animation> ().PlayQueued ("BallThrow");
+				GetComponent<Animation> ().PlayQueued ("DefaultTake");
+			}
+			else
+			{
+				move = false;
+			}
+		}
+	}
+}
