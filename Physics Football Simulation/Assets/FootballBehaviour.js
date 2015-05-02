65790b44227d3af458c7c26664614ea491b719d9
maxsegovia <maxsegoviamtz@gmail.com>
2015-05-02 16:59:28 -0500

    Minor mistakes.
    Uploaded wrong scripts.

--- Physics Football Simulation/Assets/FootballBehaviour.js
+++ Physics Football Simulation/Assets/FootballBehaviour.js
@@ -20,9 +20,10 @@ function Throw()
 {
 	
 	transform.parent = null;
-	transform.rotation = ParentBone.transform.rotation;
-	transform.rotation.x = -45;
+	transform.rotation.y = 2.3;
+	transform.rotation.z = 248;
+	transform.rotation.x = 45;
 
-	Ball.AddForce(transform.forward * -20000);
+	Ball.AddForce(transform.forward * 20000);
 	Ball.useGravity = true;
 }
